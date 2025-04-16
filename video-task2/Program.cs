6b8083cb8ee4bdc668d17d77203b2f4180f0a85a
21Umaima26 <umaimarashid1805@gmail.com>
2025-04-16 19:35:57 +0500

    Add files via upload

---
+++ video-task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("int: " + int.MinValue + " to " + int.MaxValue);
+        Console.WriteLine("float: " + float.MinValue + " to " + float.MaxValue);
+        Console.WriteLine("double: " + double.MinValue + " to " + double.MaxValue);
+        Console.WriteLine("decimal: " + decimal.MinValue + " to " + decimal.MaxValue);
+        Console.WriteLine("long: " + long.MinValue + " to " + long.MaxValue);
+        Console.WriteLine("short: " + short.MinValue + " to " + short.MaxValue);
+        Console.WriteLine("byte: " + byte.MinValue + " to " + byte.MaxValue);
+        Console.WriteLine("sbyte: " + sbyte.MinValue + " to " + sbyte.MaxValue);
+        Console.WriteLine("uint: " + uint.MinValue + " to " + uint.MaxValue);
+        Console.WriteLine("ulong: " + ulong.MinValue + " to " + ulong.MaxValue);
+        Console.WriteLine("ushort: " + ushort.MinValue + " to " + ushort.MaxValue);
+    }
+}
