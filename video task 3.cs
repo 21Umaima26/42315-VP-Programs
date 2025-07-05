db8d0bed0ea9ac184566aa8e03709be1c372e726
21Umaima26 <umaimarashid1805@gmail.com>
2025-07-05 22:24:51 +0500

    Add files via upload

---
+++ video task 3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace video_task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+            // const field
+            public const int ConstValue = 10;
+
+            // readonly field
+            public readonly int ReadOnlyValue;
+
+            // static readonly field
+            public static readonly string StaticReadOnly = DateTime.Now.ToString();
+
+            public Example(int value)
+            {
+                ReadOnlyValue = value; // OK: Can assign in constructor
+            }
+
+            static void Main()
+            {
+                Console.WriteLine("Const Value: " + ConstValue);
+                Console.WriteLine("Static ReadOnly Value: " + StaticReadOnly);
+
+                Example obj1 = new Example(25);
+                Example obj2 = new Example(50);
+
+                Console.WriteLine("obj1.ReadOnlyValue: " + obj1.ReadOnlyValue);
+                Console.WriteLine("obj2.ReadOnlyValue: " + obj2.ReadOnlyValue);
+
+                
+            }
+        }
+
+    }
+
+
---
+++ vplab.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPLAB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vIEWToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tOOLBOXToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open tool box");
+        }
+
+        private void pDFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open pdf");
+        }
+
+        private void pROJECTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open project");
+        }
+
+        private void cOPYToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and copy project");
+        }
+
+        private void pASTEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and paste project");
+        }
+
+        private void cUTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and cut project");
+        }
+
+        private void mENUToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open menu");
+        }
+
+        private void tOOLSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open tools");
+        }
+
+        private void dESINGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click and open desing");
+        }
+    }
+}
