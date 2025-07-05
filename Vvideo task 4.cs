9482a55ae9018c397e99fda2840e571f768d0183
21Umaima26 <umaimarashid1805@gmail.com>
2025-07-05 22:25:46 +0500

    Add files via upload

---
+++ Vvideo task 4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace video_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            // Global variable (class-level)
+            int globalCounter = 0;
+
+            public void IncreaseCounter()
+            {
+                // Local variable
+                int localCounter = 0;
+
+                globalCounter++;
+                localCounter++;
+
+                Console.WriteLine("Global Counter: " + globalCounter);
+                Console.WriteLine("Local Counter: " + localCounter);
+            }
+
+            static void Main()
+            {
+                VariableScope obj = new VariableScope();
+
+                Console.WriteLine("First Call:");
+                obj.IncreaseCounter();
+
+                Console.WriteLine("\nSecond Call:");
+                obj.IncreaseCounter();
+            }
+        }
+
+    }
+
+
